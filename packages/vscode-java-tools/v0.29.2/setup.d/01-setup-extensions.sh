d990be6ffa85da8b1b2edec0a771603c9b7ef21b
Bill Kable <bill.kable@spinguard.io>
2025-06-06 12:03:21 -0500

    fix platform arch specific package install

--- packages/vscode-java-tools/v0.29.2/setup.d/01-setup-extensions.sh
+++ packages/vscode-java-tools/v0.29.2/setup.d/01-setup-extensions.sh
@@ -6,8 +6,14 @@ set -eo pipefail
 PACKAGE_DIR=$(cd "$(dirname $BASH_SOURCE)/.."; pwd)
 
 # Install extensions
-code-server --install-extension $PACKAGE_DIR/extensions/redhat.java-amd64/java-linux-x64-1.42.0-561.vsix
-code-server --install-extension $PACKAGE_DIR/extensions/redhat.java-arm64/java-linux-arm64-1.42.0-561.vsix
+
+## If $PLATFORM_ARCH is set to "arm64", use the arm64 versions of the extensions
+if [[ "$PLATFORM_ARCH" == "arm64" ]]; then
+    code-server --install-extension $PACKAGE_DIR/extensions/redhat.java-arm64/java-linux-arm64-1.42.0-561.vsix
+else
+    code-server --install-extension $PACKAGE_DIR/extensions/redhat.java-amd64/java-linux-x64-1.42.0-561.vsix
+fi
+
 code-server --install-extension $PACKAGE_DIR/extensions/vscjava.vscode-java-debug/vscjava.vscode-java-debug-0.58.2.vsix
 code-server --install-extension $PACKAGE_DIR/extensions/vscjava.vscode-java-test/vscjava.vscode-java-test-0.43.1.vsix
 code-server --install-extension $PACKAGE_DIR/extensions/vscjava.vscode-maven/vscjava.vscode-maven-0.44.0.vsix
